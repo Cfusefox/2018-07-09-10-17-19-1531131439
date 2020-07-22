0561720ed8d0e1aa9152d3ca44f66cd651197cc5
YORK ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGYO5@OOCL.COM>
2020-07-22 16:26:51 +0800

    [York] feature: complete practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,47 @@
 package practice09;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> memberList;
+
+    public Klass(int number) {
+        this.number = number;
+        this.memberList = new ArrayList<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if(memberList.indexOf(student) < 0) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = student;
+        }
+    }
+    public void appendMember(Student student) {
+        this.memberList.add(student);
+    }
 }
