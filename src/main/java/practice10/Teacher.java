9b08c7281679f25a6979058200d4b6569ee243fb
YORK ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGYO5@OOCL.COM>
2020-07-22 18:09:05 +0800

    [York] feature: complete practice10

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,63 @@
 package practice10;
 
-public class Teacher {
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Teacher extends Person{
+    private List<Klass> classes = new ArrayList<>();
+    private List<Integer> classNumList = new ArrayList<>();
+    private List<String> memberList = new ArrayList<>();
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes.addAll(classes);
+        Iterator it = classes.iterator();
+        while (it.hasNext()) {
+            Klass obj = (Klass) it.next();
+            classNumList.add(obj.getNumber());
+        }
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<Klass> classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String introduce() {
+        if(classes.size() > 0) {
+            return super.introduce() + " I am a Teacher. I teach Class " + classNumList.toString() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if(classNumList.indexOf(student.getKlass().getNumber()) >= 0) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
+
+    public boolean isTeaching(Student student) {
+        Iterator it = this.classes.iterator();
+        while(it.hasNext()) {
+            Klass obj = (Klass) it.next();
+            if(obj.isIn(student) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
